9689d9ecc3bebdcb262c0424742097c68772738c
Your Name <you@example.com>
2024-05-20 01:25:40 +0000

    new try

--- sudoku.c
+++ sudoku.c
@@ -44,8 +44,21 @@ void print_node(Node* n){
 }
 
 int is_valid(Node* n){
-
-    return 1;
+   for (int i = 0; i < 9; i++) {
+      for (int j=0; j < 9; j++) {
+         if (n->sudo[i][j] != 0) {
+            for (int k = 0; k < 9; k++) {
+               if (n->sudo[i][k] == n->sudo[i][j] && k != j) {
+                  return 0;
+               }
+               if (n->sudo[k][j] == n->sudo[i][j] && k != i) {
+                  return 0;
+               }
+            }
+         }
+      }
+   }
+   return 1;
 }
 
 List* get_adj_nodes(Node* n){
